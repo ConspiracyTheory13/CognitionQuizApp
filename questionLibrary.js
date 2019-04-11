6e6ab2e879f5f36fbf8fcff12fbd73f44fd20bfc
Babylon <xbettybabylonx@gmail.com>
2019-04-11 15:55:02 -0700

    questionstore that I forgot last push

---
+++ questionLibrary.js
@@ -0,0 +1,78 @@
+Question 1:  You value your status as an informed person who always watches the news and stays up to date on local and world politics via the internet and social media. However, you are often afraid and imagine that crime rates are out of control. Your mind is using the:
+
+Answers,
+A. Depressive Realism heuristic.
+B. Availability heuristic.
+C. Representativeness heuristic.
+D. Anchoring and Adjustment heuristic
+
+Question 2:  Certain words and phrases have powerful emotional affects, when your stomache clenches in dread at the word “cancer” or you feel a sense of unease from the word “death”. What shortcut is your mind using?
+
+Answers,
+A. Affect heuristic
+B. Terror Management Theory
+C. Scapegoat Theory
+D. Gut Reaction heuristic
+
+Question 3: You and your buddy Larry are flipping coins for a probability experiment. Somehow you’ve come up with heads five times in a row! “That’s it,” Larry says. “The next one has got to be tails. It’s statistically impossible!”. Larry is using the:
+
+Answers,
+A. Law of the instrument effect
+B. Statistical Bias
+C. Gambler’s Fallacy
+D. 50/50 Theory
+
+Question 4: The speaker at the cat flatterer convention is asking for audience members to guess the value of an ultra deluxe set of premium cat furniture. “How much do you think it’s worth?” he asks the audience. “At least 3k, right?” the tendency for the audience to then venture guesses closer in range to the speakers represents the:
+
+Answers,
+A.  Attentional Bias
+B.  Fluency Heuristic
+C. Anchoring Heuristic
+D. Speaker Fallacy
+
+Question 5: The tendency for people to value that which is communicated more skillfully or elegantly represents the:
+
+A. Anchoring Heuristic
+B. Fluency Heuristic
+C. Brain in a Vat
+D. Same Bias
+
+Question 6: You watch with baited breath as your race horse glides neck and neck with another, round and around the track. On the final lap your house begins to lag, but at the stretch makes a miraculous recovery. Nonetheless, your horse still loses. “It would have been better if it hadn’t been close at all!” you lament, racking your brain over all the small differences that would have let your horse win. This is an example of the:
+
+A.  Simulation heuristic.
+B. Gambler’s Fallacy
+C. Impact Bias
+D. Learned Helplessness
+
+
+
+
+
+Question 7:
+The tendency to ignore (generic, general information) and focus on specific information is the:
+
+A.  Simulation heuristic.
+B. Gambler’s Fallacy
+C. Impact Bias
+D. Base rate fallacy
+
+Question 8: The tendency for a person to recognize and remember details that confirm preconceived notions is an example of:
+
+A.  Simulation heuristic.
+B. Observer-expectancy Effect
+C. Optimism Bias
+D. Confirmation bias
+
+Question 9: The tendency to prescribe a person’s apparent behavior to their personality and to ignore circumstance is the:
+A.  Simulation heuristic.
+B. Gambler’s Fallacy
+C. Fundamental Attribution Error
+D. Learned Helplessness
+
+
+Question 10: The
+
+A.  Outcome Bias
+B. Normalcy Bias
+C. Omission Bias
+D. Learned Helplessness
