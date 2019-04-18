5ae04a2786c13f79e6a2f19931583d866a921cde
Babylon <xbettybabylonx@gmail.com>
2019-04-18 14:26:09 -0700

    help with next button

--- index.js
+++ index.js
@@ -1,45 +1,115 @@
-'use strict'
+// 'use strict'
 $(document).ready(function() {
+     let questionNumber = 0;
+     let score = 0;
 
-     let questionForm ='<form role="questionandanswers">' +
-               '<h1></h1>' +
-               '<input type="radio" name="answer" value="" checked> Call answer<br>' +
-               '<input type="radio" name="answer" value=""> Call answer<br>' +
-               '<input type="radio" name="answer" value=""> Call answer<br>' +
-               '<input type="radio" name="answer" value=""> Call answer<br>' +
-               '</form>' +
+     let questionForm =
+               '<form role="questionandanswers">' +
+               '<h1>' + STORE[0].question + '</h1>' +
+               '<input type="radio" class= "answerOption" name="answer" value="" checked>' + STORE[questionNumber].answers[0] + '<br>' +
+               '<input type="radio" class= "answerOption" name="answer" value="">' + STORE[questionNumber].answers[1] + '<br>' +
+               '<input type="radio" class= "answerOption" name="answer" value="">' + STORE[questionNumber].answers[2] + '<br>' +
+               '<input type="radio" class= "answerOption" name="answer" value="">' + STORE[questionNumber].answers[3] + '<br>' +
+               '</form>'+
                '<button type="button" class="bttn" id="#nextBttn">Next Question!</button>'
 
-     if (questionNumber < STORE.length) {
-          return `<div class="question-${questionNumber}">
-                 <h1>${STORE[questionNumber].question}</h1>
-                 <form>
-                 <fieldset>
-                   <label class="answerOption">
-                   <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required>
-                   <span>${STORE[questionNumber].answers[0]}</span>
-                   </label>
-                   <label class="answerOption">
-                   <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
-                   <span>${STORE[questionNumber].answers[1]}</span>
-                   </label>
-                   <label class="answerOption">
-                   <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required>
-                   <span>${STORE[questionNumber].answers[2]}</span>
-                   </label>
-                   <label class="answerOption">
-                   <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required>
-                   <span>${STORE[questionNumber].answers[3]}</span>
-                   </label>
-                   <button type="submit" class="submitButton">Submit</button>
-                   </fieldset>
-                   </form>
-                   </div>`;
-    } else {
-    renderResults();
-    restartQuiz();
-    $('.questionNumber').text(10);
+
+     $('.startBttn').on("click",(function(event){
+          $('#answerFormRender').append(questionForm);
+          $("#answerFormRender").modal({
+                    fadeDuration: 100,
+                    closeExisting: false,
+                    escapeClose: false,
+                    clickClose: false,
+                    showClose: false
+          });
+     }));
+
+  //    function nxtButton() {
+  //    $('.main').on('click', '#nextBttn', function(event) {
+  //        console.log('nextbuttonfires');
+  //    });
+  // }
+
+
+function nxtButton() {
+     $('main').on("click", '.nextBttn' (function(event){
+          console.log('nextbuttonfires');
+          e.preventDefault()
+          questionNumber ++;
+          questionForm = '<form id"questionAnswers" role="questionandanswers">' +
+          '<h1>' + STORE[questionNumber].question + '</h1>' +
+          '<input type="radio" class= "answerOption" name="answer" value="" checked>' + STORE[questionNumber].answers[0] + '<br>' +
+          '<input type="radio" class= "answerOption" name="answer" value="">' + STORE[questionNumber].answers[1] + '<br>' +
+          '<input type="radio" class= "answerOption" name="answer" value="">' + STORE[questionNumber].answers[2] + '<br>' +
+          '<input type="radio" class= "answerOption" name="answer" value="">' + STORE[questionNumber].answers[3] + '<br>' +
+          '</form>'
+          $("#answerFormRender").html(questionForm);
+     }));
+}
+
+     function buildQuestion() {
+
+          if (questionNumber < STORE.length) {
+               questionNumber ++;
+               return  `<div class="question-${questionNumber[0]}">
+                        <h1>${STORE[questionNumber].question}</h1>
+                        <form>
+                        <fieldset>
+                        <label class="answerOption">
+                        <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required>
+                        <span>${STORE[questionNumber].answers[0]}</span>
+                        </label>
+                        <label class="answerOption">
+                        <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
+                        <span>${STORE[questionNumber].answers[1]}</span>
+                        </label>
+                        <label class="answerOption">
+                        <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required>
+                        <span>${STORE[questionNumber].answers[2]}</span>
+                        </label>
+                        <label class="answerOption">
+                        <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required>
+                        <span>${STORE[questionNumber].answers[3]}</span>
+                        </label>
+                        <button type="submit" class="submitButton">Submit</button>
+                        </fieldset>
+                        </form>
+                        </div>`;
+         } else {
+         renderResults();
+         restartQuiz();
+         $('.questionNumber').text(10);
+         }
+}
+
+function advanceQuestionNumber () {
+     questionNumber ++;
+     $('.questionNumber').text(questionNumber+1);
+}
+
+function updateScore() {
+     score++;
+}
+
+function callQuestion () {
+     $('.questionAnswerForm').html(buildQuestion());
 };
+     console.log("Modalrunning");
+
+     // function renderQuiz () {
+     //   startQuiz();
+     //   renderQuestion();
+     //   userSelectAnswer();
+     //   renderNextQuestion();
+     // }
+     //
+     // $(renderQuiz);
+
+});
+
+
+
 
 
 //                }
@@ -66,130 +136,6 @@ $(document).ready(function() {
 //use an if else loop, for scores above half go to one screen, for scores below half go
 //to another. Regardless of score- a restart button is neccessary.
 
-     $('.startBttn').on("click",(function(event){
-          $('#answerFormRender').append(questionForm
-          );
-
-//render the modal
-          $("#answerFormRender").modal({
-               fadeDuration: 100,
-               closeExisting: false,
-               escapeClose: false,
-               clickClose: false,
-               showClose: false
-
-          });
-     }));
-     console.log("Modalrunning");
-
-
-     const STORE = [
-          {
-               'question': 'You value your status as an informed person who always watches the news and stays up to date on local and world politics via the internet and social media. However, you are often afraid and imagine that crime rates are out of control. Your mind is using the:',
-               'answers': [
-                    'Depressive Realism heuristic.',
-                    'Availability heuristic.',
-                    'Representativeness heuristic.',
-                    'Anchoring and Adjustment heuristic'
-               ],
-               'correctAnswer': 'Availability heuristic.'
-               // ,icon: somethingcooloragif.png,
-               // alt: 'idk'
-          },
-          {
-               question: 'Certain words and phrases have powerful emotional affects, when your stomache clenches in dread at the word “cancer” or you feel a sense of unease from the word “death”. What shortcut is your mind using?',
-               answers: [
-                    'Affect heuristic.',
-                    'Terror Management Theory',
-                    'Scapegoat Theory',
-                    'Gut Reaction heuristic'
-               ],
-               correctAnswer: 'Affect heuristic.'
-          },
-          {
-               question: 'You and your buddy Larry are flipping coins for a probability experiment. Somehow you’ve come up with heads five times in a row! “That’s it,” Larry says. “The next one has got to be tails. It’s statistically impossible!”. Larry is using the:',
-               answers: [
-                    'Law of the instrument effect',
-                    'Statistical Bias',
-                    'Gambler’s Fallacy',
-                    '50/50 Theory'
-               ],
-               correctAnswer: 'Gambler\'s Fallacy'
-          },
-          {
-               question: 'The speaker at the cat flatterer convention is asking for audience members to guess the value of an ultra deluxe set of premium cat furniture. “How much do you think it’s worth?” he asks the audience. “At least 3k, right?” the tendency for the audience to then venture guesses closer in range to the speakers represents the:',
-               answers: [
-                    'Attentional Bias',
-                    'Fluency Heuristic',
-                    'Anchoring Heuristic',
-                    'Speaker Fallacy'
-               ],
-               correctAnswer: 'Anchoring Heuristic',
-          },
-          {
-               question: 'The tendency for people to value that which is communicated more skillfully or elegantly represents the:',
-               answers: [
-                    'Anchoring Heuristic',
-                    'Fluency Heuristic',
-                    'Brain in a Vat',
-                    'Same Bias'
-               ],
-               correctAnswer: 'Fluency Heuristic',
-          },
-          {
-               question: 'The tendency to ignore (generic, general information) and focus on specific information is the:',
-               answers: [
-                    'Simulation heuristic.',
-                    'Gambler’s Fallacy',
-                    'Impact Bias',
-                    'Base rate fallacy'
-               ],
-               correctAnswer: 'Base rate fallacy',
-          },
-          {
-               question: 'You watch with baited breath as your race horse glides neck and neck with another, round and around the track. On the final lap your house begins to lag, but at the stretch makes a miraculous recovery. Nonetheless, your horse still loses. “It would have been better if it hadn’t been close at all!” you lament, racking your brain over all the small differences that would have let your horse win. This is an example of the:',
-               answers: [
-                    'Simulation heuristic',
-                    'Gambler’s Fallacy',
-                    'Impact Bias',
-                    'Learned Helplessness'
-               ],
-               correctAnswer: 'Simulation heuristic',
-          },
-          {
-               question: 'The tendency for a person to recognize and remember details that confirm preconceived notions is an example of:',
-               answers: [
-                    'Simulation heuristic.',
-                    'Observer-expectancy Effect',
-                    'Optimism Bias',
-                    'Confirmation bias'
-               ],
-               correctAnswer: 'Confirmation bias',
-          },
-          {
-               question: 'The tendency to prescribe a person’s apparent behavior to their personality and to ignore circumstance is the:',
-               answers: [
-                    'Simulation heuristic.',
-                    'Gambler’s Fallacy',
-                    'Fundamental Attribution Error',
-                    'Learned Helplessness',
-
-               ],
-               correctAnswer: 'Fundamental Attribution Error',
-          },
-          {
-               question: 'The tendency for an individual to attribute the overall validity of a decision based on the eventual outcome is known as',
-               answers: [
-                    'Outcome Bias',
-                    'Normalcy Bias',
-                    'Omission Bias',
-                    'Learned Helplessness'
-               ],
-               correctAnswer: 'Outcome Bias',
-          }
-     ];
-
-});
      // $().append()
 
 // 'modal:before-open';
