0b97ce74132ca683af908e4dfeab863cfb610ab6
Babylon <xbettybabylonx@gmail.com>
2019-05-11 23:13:46 -0700

    !

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { STORE } from './questionLibrary.js';
 
 const state = {
-  score: 0,
+  // score: 0;
   incorrectScore: 0,
-  questionNumber: 0,
   correctScore: 0,
+  questionNumber: 0,
   currentQuestion: 0
 };
 
@@ -24,13 +24,39 @@ test && assert({
   expected: 'ASIDE',
 });
 
+const response = (textFeedback,questionIndex) => {
+  const content = `<p>${textFeedback}</p><button type="button" id="nextBttn" class="nextBttn"
+   value="${questionIndex}">Next Question!</button>`
+  const template = document.createElement('template');
+  template.innerHTML = content;
+  return template;  
+};
+
+
+const renderFeedback = (event) => {
+  const target = event.currentTarget;
+  const questionIndex = target.value;
+  const selectedAnswer = target.parentElement.querySelector(`input[name='question-${questionIndex}']:checked`).value;
+  const correctAnswer = STORE[questionIndex].correctAnswer;
+  const answerIsTrue = compareQuestion(selectedAnswer, correctAnswer);
+  const textFeedback = feedback(answerIsTrue, correctAnswer);
+  incrementScore(answerIsTrue);
+  render({ template: response(textFeedback,questionIndex), container: 'quizQuestions'});
+  };
+
+
 const render = ({template, container}) => {
   const tpl = typeof template === 'string'? $(template) : template;
   const root = $(container)
   while (root.firstChild) {
     root.removeChild(root.firstChild);
   }
-  root.append(tpl.cloneNode(true).content);
+
+  root(tpl.cloneNode(true).content);
+  const nextBttn = $('nextBttn');
+  const answerBttn = $('answerBttn')
+  if (nextBttn) nextBttn.addEventListener('click', questions)
+  if (answerBttn) answerBttn.addEventListener('click', renderFeedback)
   return root;
 };
 
@@ -56,18 +82,71 @@ test && assert({
   expected: $('quizModalContainer').firstChild,
 });
 
+const compareQuestion = (selectedAnswer, correctAnswer) => {
+  return selectedAnswer === correctAnswer;
+};
+
+const incrementScore = (answerIsTrue) => {
+answerIsTrue ? state.correctScore++ : state.incorrectScore++;
+// above is an else if statement, the first half before the : is the if, the incorrectScore is the else.
+};
+
+const feedback = (answerIsTrue, correctAnswer) => {
+const questionCorrect= `Correct~`
+const questionIncorrect= `Sorry incorrect! The correct answer is: ${correctAnswer}`; 
+return (answerIsTrue ? questionCorrect : questionIncorrect);
+};
+
+
+//currently working on...
+
+  // const submitBttn = $('submitBttn');
+  // const reloadBttn = $('reloadBttn');
+  // if (submitBttn) subbmitBttn.addEventListener('click', renderQuizFeedback);
+  // if (reloadBttn) reloadBttn.addEventListener('click', reloadQuiz);
+
+//renderQuizFeedback needs to contain each variable for grades
+// it's one job is to fire on click of submit button, take correctScore, and return score.
+
+// const renderQuizFeedback (correctScore) => {
+//   const perfectGrade=`Congratulations! You scored 10/10!`;
+//   const goodGrade = `Congratulations! You scored ${correctScore} and ${incorrectScore}` ;
+//   const badGrade = `Good try, today you scored ${correctScore} and ${incorrectScore}!`;
+
+//   if (correctScore === 10) {
+//     reloadQuiz();
+//     return perfectGrade;
+
+//   } else if (correctScore <= 6) {
+//     reloadQuiz();
+//     return goodGrade;
+
+//   } else {
+//     reloadQuiz();
+//     return badGrade;
+//   }
+// }
+// 
+//reloadQuiz has to render a restart button that reloads
+// const reloadQuiz() => {
+//   const reloadBttn =`<button type="button" id="reloadBttn" class="reloadBttn"
+//    value="">Play again?</button>`
+  
+// }
+// currently working on....
+
 const questions = () => {
   const content = STORE.map(({question, answers}, questionIndex) => `<div class="${['question', (questionIndex === 0) && 'visible'].filter(Boolean).join(' ')}" id='question-${questionIndex}'>
     <p class='question-copy'>${question}</p>
     ${answers.map((answer, answerIndex) => `<input type="radio" class="answerOption" name="question-${questionIndex}" id='${answerIndex}-${answerIndex}' value="${answer}" />
     <label class="label" for="${answerIndex}-${answerIndex}">${answer}</label>  
       `).join('\n')}
-      <button type="button" class="nextBttn" value="${questionIndex}">Next Question!</button>
+      <button type="button" id="answerBttn" class="nextBttn" value="${questionIndex}">Next Question!</button>
     </div>`).join('\n');
   const template = document.createElement('template');
   template.innerHTML = content;
   return template;  
-}
+};
 test && assert({
   given: 'question function call',
   should: 'first child has class visible',
@@ -82,39 +161,10 @@ test && assert({
   expected: false,
 });
 
-//dynamically cr5eate a template
-//turning contetnt into the inner HTML of a template element created with js
-
-
-
-
-
-
-
 const startBttn = $('startBttn');
 const renderModalGuts = () => {
   render({ template:'modalGutsTemplate', container: 'quizModalContainer'});
   render({ template: questions(), container: 'quizQuestions'});
 };
-startBttn.addEventListener('click', renderModalGuts);
 
-// const quizModalContainer = document.getElementById('quizModalContainer');
-// const modalGutsTemplate = document.getElementById('modalGutsTemplate');
-// const startButton = () => {
-//   quizModalContainer.append(document.cloneNode(modalGutsTemplate.content, true));
-// };
-//
-//
-//
-// const modalGuts = () => {
-//
-//
-//   const questionForm = document.querySelector('#questionForm');
-//   const questionFormClone = document.cloneNode(questionForm.content, true);
-//   const nextButton = document.querySelector('#nextButton');
-//   const nextButtonClone = document.cloneNode(nextButton.content, true);
-//   clone.querySelector("#score").appendChild(state.score);
-//   clone.querySelector("#questionAnswer").appendChild(questionFormClone);
-//   clone.querySelector("#nextButton").appendChild(state.score);
-//   return clone;
-// }
+startBttn.addEventListener('click', renderModalGuts);
